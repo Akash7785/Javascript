3199dc424e4eadae37d23b7c0cbb02313cdb0cdb
Akash <akashks838@gmail.com>
2023-05-16 09:44:38 +0530

    add

---
+++ 01Programs/Pattern/p33.js
@@ -0,0 +1,10 @@
+function getFileExtension(filename) {
+  const extension = filename.split(".").pop();
+  return extension;
+}
+
+const result1 = getFileExtension("module.js");
+console.log(result1);
+
+const result2 = getFileExtension("module.txt");
+console.log(result2);
