a28b71f633440eb101ed2667dfb4a4c1c3079363
Akash <akashks838@gmail.com>
2023-06-07 23:31:32 +0530

    add

---
+++ 01Programs/Pattern/p053.js
@@ -0,0 +1,9 @@
+// string definition
+const greeting = "Good morning!";
+
+// UTF-16 code unit of character at index 5
+let result = greeting.charCodeAt(5);
+
+console.log(result);
+
+// Output: 109
--- 01Programs/Pattern/p53.js
+++ 01Programs/Pattern/p53.js
@@ -2,4 +2,4 @@ const string = prompt("Enter a character: ");
 
 const result = string.charCodeAt(0);
 
-console.log(`The ASCII value is: ${result}`);
+console.log(` ASCII value is: ${result}`);
